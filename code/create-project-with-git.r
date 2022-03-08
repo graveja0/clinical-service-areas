a7f87694cf952a64aeaf8e52f5f1ccb1d6fcc5f5
graveja0 <john.graves@vanderbilt.edu>
2022-03-08 11:32:18 -0600

    initial commmit

---
+++ code/create-project-with-git.r
@@ -0,0 +1,9 @@
+# Steps to create a new project
+
+# 1. Create a directory on Box
+# 2. Create a new Rstudio project in this directory.
+# 3. Create any necessary subdirectories.
+# 4. Set up git using the following command: 
+
+# usethis::use_git()
+
